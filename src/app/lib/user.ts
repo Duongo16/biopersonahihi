a62de47586bed3da4cb3a7207e5e086cfbfde5b7
duongo_16 <duongo1604@gmail.com>
2025-06-05 14:51:11 +0700

    fix dashboard user

--- src/app/lib/user.ts
+++ src/app/lib/user.ts
@@ -11,10 +11,16 @@ export interface CCCDInfo {
 
 export async function getCCCDInfo(): Promise<CCCDInfo | null> {
   try {
-    const res = await fetch("/api/ekyc/cccd/info", {
-      method: "GET",
-      credentials: "include",
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_EKYC_API}ekyc/cccd-info`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
 
     if (!res.ok) return null;
 
